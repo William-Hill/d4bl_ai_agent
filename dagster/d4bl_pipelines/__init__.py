1ec3ef7369fb732431f95f23085262dc26201c13
William Hill <mjh2225@gmail.com>
2026-03-11 14:47:04 -0400

    feat: wire static schedules into Dagster Definitions (#73)

--- dagster/d4bl_pipelines/__init__.py
+++ dagster/d4bl_pipelines/__init__.py
@@ -2,21 +2,25 @@ import logging
 
 from d4bl_pipelines import assets as asset_modules
 from d4bl_pipelines.resources import get_db_url, get_resources
-from d4bl_pipelines.schedules import load_schedules_from_db
+from d4bl_pipelines.schedules import build_static_schedules, load_schedules_from_db
 from dagster import Definitions, load_assets_from_modules
 
 logger = logging.getLogger(__name__)
 
 all_assets = load_assets_from_modules([asset_modules])
 
+static_schedules = build_static_schedules()
+
 try:
-    schedules = load_schedules_from_db(get_db_url())
+    db_schedules = load_schedules_from_db(get_db_url())
 except Exception:
     logger.warning(
         "Failed to load schedules from DB; starting with none",
         exc_info=True,
     )
-    schedules = []
+    db_schedules = []
+
+schedules = static_schedules + db_schedules
 
 defs = Definitions(
     assets=all_assets,
